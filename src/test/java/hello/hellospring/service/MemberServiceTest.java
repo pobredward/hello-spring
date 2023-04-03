f8154be0b5d75ac9e1f0d087b20c7574bdb96fb4
Edward Pobre <100953765+pobredward@users.noreply.github.com>
2023-04-04 00:00:56 +0900

    Update MemberServiceTest.java

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,13 +1,62 @@
 package hello.hellospring.service;
 
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+
     @Test
     void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    void 중복회원받기() {
+        Member member1 = new Member();
+        member1.setName("hihi");
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("byebye");
+        memberService.join(member2);
+
+        Member member3 = new Member();
+        member3.setName("hihi");
+        memberService.join(member3);
+
     }
 
     @Test
